fd2cc1751afad02f4817075ffe4a4972e19ebf72
Konstantin S <havendv@gmail.com>
2025-01-07 22:32:56 +0400

    fix: Fixed

--- src/libs/LangSmith/Types.cs
+++
@@ -1,9 +0,0 @@
-namespace LangSmith;
-
-public sealed partial class JsonSerializerContextTypes
-{
-    /// <summary>
-    /// 
-    /// </summary>
-    public global::System.Text.Json.JsonElement JsonElement { get; set; }
-}
