bb4aace26911759355ef83da20edf78ded4be981
Konstantin S. <havendv@gmail.com>
2026-04-03 09:06:50 +0400

    feat: Updated OpenAPI spec (#402)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/LangSmith/Generated/LangSmith.AlertRulesClient.DeleteAnAlertRule.g.cs
+++ src/libs/LangSmith/Generated/LangSmith.AlertRulesClient.DeleteAnAlertRule.g.cs
@@ -313,7 +313,6 @@ namespace LangSmith
                 try
                 {
                     __response.EnsureSuccessStatusCode();
-
                     var __content = await __response.Content.ReadAsStringAsync(
 #if NET5_0_OR_GREATER
                         cancellationToken
--- src/libs/LangSmith/Generated/LangSmith.ExperimentsClient.EvaluateExperimentAdhoc.g.cs
+++ src/libs/LangSmith/Generated/LangSmith.ExperimentsClient.EvaluateExperimentAdhoc.g.cs
@@ -184,7 +184,6 @@ namespace LangSmith
                 try
                 {
                     __response.EnsureSuccessStatusCode();
-
                     var __content = await __response.Content.ReadAsStringAsync(
 #if NET5_0_OR_GREATER
                         cancellationToken
